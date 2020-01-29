5d1042ae1201da128a819a4eee05f59fe26c3f75
Brandon <yberion.ins@gmail.com>
2020-01-29 16:23:18 +0100

    Travis - General update
    - Update GCC to version 9 on Linux
    - Update LLVM to lastest supported version (actually 9) on Linux
    - Update CMake to the lastest available version on Homebrew and chocolatey
    - Manually update CMake to 3.16.3 on Linux (amd64)
    
    Commenting out the following builds from the process because there is no real need for them:
    
    - (Release) x64 [clang 9] - Ubuntu 18.04
    - (Release) arm64 [clang 9] - Ubuntu 18.04
    - (Release) x86 [clang 9] - Ubuntu 18.04
    - (Debug) arm64 [GCC 9] - Ubuntu 18.04
    - (Debug) x64 [clang 9] - Ubuntu 18.04
    - (Debug) arm64 [clang 9] - Ubuntu 18.04
    - (Debug) x64 [Apple clang version 11.0.0] - macOS 10.14
    - (Debug) x86 [clang 9] - Ubuntu 18.04
    
    CMake version on arm64 is 3.10.2, update in further commits.

--- continuous_integration/travis/install/linux.sh
+++ continuous_integration/travis/install/linux.sh
@@ -5,17 +5,43 @@ set -v
 set -x
 
 ARCH="$1"
-
-sudo apt-get update -yq
+COMPILER="$2"
+CPU_ARCH="$3"
 
 APT_GET_INSTALL='sudo -E apt-get -yq --no-install-suggests --no-install-recommends install'
 
+if [ "${CPU_ARCH}" = "amd64" ]; then
+	sudo mkdir -p cmake-3.16.3 && sudo wget -qO- "https://cmake.org/files/v3.16/cmake-3.16.3-Linux-x86_64.tar.gz"  | sudo tar --strip-components=1 -xz -C cmake-3.16.3 ;
+fi
+
+case "${COMPILER}" in
+	gcc)
+		;;
+
+	clang)
+		sudo bash -c "$(wget -O - https://apt.llvm.org/llvm.sh)"
+		;;
+esac
+
+sudo apt-get update -yq
+
 case "${ARCH}" in
 	x64)
-		${APT_GET_INSTALL} libsdl2-dev
+		${APT_GET_INSTALL} \
+			gcc-9 \
+			g++-9 \
+			libsdl2-dev
 		;;
 
 	x86)
-		${APT_GET_INSTALL} libc6:i386 libstdc++6:i386 gcc-multilib g++-multilib zlib1g-dev:i386 libpng-dev:i386 libjpeg-turbo8-dev:i386 libsdl2-dev:i386
+		${APT_GET_INSTALL} \
+			gcc-9-multilib \
+			g++-9-multilib \
+			libc6:i386 \
+			libstdc++6:i386 \
+			zlib1g-dev:i386 \
+			libpng-dev:i386 \
+			libjpeg-turbo8-dev:i386 \
+			libsdl2-dev:i386
 		;;
 esac
